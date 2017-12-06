86ddf9fd6775d434406fb66d9d2b2e296f9f2c4b
DESKTOP-V5KVRI6\Dylan <dylan_verstraete1@hotmail.com>
2017-12-06 14:06:29 +0100

    prepare for heroku

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -7,19 +7,19 @@ export class AdminService {
   constructor(private http: Http) { }
   
     getAllUser() {
-      return this.http.get('http://127.0.0.1:5000/api/users/' , this.jwt()).map((response: Response) => response.json());
+      return this.http.get('/api/users/' , this.jwt()).map((response: Response) => response.json());
     }
 
     addCardToUserWithId(id: number){
-      return this.http.post('http://127.0.0.1:5000/api/users/addCard/' + id , this.jwt()).map((response: Response) => response.json());
+      return this.http.post('/api/users/addCard/' + id , this.jwt()).map((response: Response) => response.json());
     }
 
     addMember(user: User){
-      return this.http.post('http://127.0.0.1:5000/api/add/member/', user , this.jwt()).map((response: Response) => response.json());
+      return this.http.post('/api/add/member/', user , this.jwt()).map((response: Response) => response.json());
     }
 
     editTurn(turn: any){
-      return this.http.post('http://127.0.0.1:5000/api/editTurn/' , turn , this.jwt()).map((response: Response) => response.json());
+      return this.http.post('/api/editTurn/' , turn , this.jwt()).map((response: Response) => response.json());
     }
     // private helper methods
   
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,50 +9,50 @@ export class UserService {
     constructor(private http: Http) { }
 
     getAll() {
-        return this.http.get('http://127.0.0.1:5000/api/users', this.jwt()).map((response: Response) => response.json());
+        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
     }
 
     
     getByEmail(id: number) {
-        return this.http.get('http://127.0.0.1:5000/api/users' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.get('/api/users' + id, this.jwt()).map((response: Response) => response.json());
     }
     
 
     getCurrentUser(){
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        return this.http.get("http://127.0.0.1:5000/api/users/" + currentUser.email, this.jwt()).map((response: Response) => response.json());
+        return this.http.get("/api/users/" + currentUser.email, this.jwt()).map((response: Response) => response.json());
     }
 
     create(user: User) {
-        return this.http.post('http://127.0.0.1:5000/api/signup', user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('/api/signup', user, this.jwt()).map((response: Response) => response.json());
     }
     
     update(user: User) {
-        return this.http.post('http://127.0.0.1:5000/api/users/edit/' + user.email, user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('/api/users/edit/' + user.email, user, this.jwt()).map((response: Response) => response.json());
     }
 
     delete(id: number) {
-        return this.http.delete('http://127.0.0.1:5000/api/users' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.delete('/api/users' + id, this.jwt()).map((response: Response) => response.json());
     }
 
     requestSecretRoute(){
-        return this.http.get("http://127.0.0.1:5000/api/secret", this.jwt()).map((response: Response) => response.json());
+        return this.http.get("/api/secret", this.jwt()).map((response: Response) => response.json());
     }
 
     addSkiCard(user: User){
-        return this.http.post('http://127.0.0.1:5000/api/users/addCard' , user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('/api/users/addCard' , user, this.jwt()).map((response: Response) => response.json());
     }
 
     requestCard(user: User){
-        return this.http.post('http://127.0.0.1:5000/api/users/requestCard' , user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('/api/users/requestCard' , user, this.jwt()).map((response: Response) => response.json());
     }
 
     makeMember(user: User){
-        return this.http.post('http://127.0.0.1:5000/api/add/pendingMember' , user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('/api/add/pendingMember' , user, this.jwt()).map((response: Response) => response.json());
     }
 
     undoMember(user: User){
-        return this.http.post('http://127.0.0.1:5000/api/delete/member' , user, this.jwt()).map((response: Response) => response.json());        
+        return this.http.post('/api/delete/member' , user, this.jwt()).map((response: Response) => response.json());        
     }
 
     // private helper methods
