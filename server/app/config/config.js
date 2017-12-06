93bcb99f4071bca3792e778a528d77445658928d
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2017-12-06 14:55:19 +0100

    test

--- server/app/config/config.js
+++ server/app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'OVSWRzBeL4PFgs6smfEWl7btuyXACZR7aByLMwwu9bwXh4BYlrBW',
-    'database': 'mongodb://IceCold:tcoms99@ds129936.mlab.com:29936/skischool"'
+    'database': 'mongodb://IceCold:tcoms99@ds129936.mlab.com:29936/skischool'
 }
   
