c758d108684a27140342c9c0ed9590af20699c11
Dylan Verstraete <dylan_verstraete1@hotmail.com>
2017-12-07 20:35:03 +0100

    try bugfix auth

--- server/app/config/config.js
+++ server/app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'secret': 'OVSWRzBeL4PFgs6smfEWl7btuyXACZR7aByLMwwu9bwXh4BYlrBW',
+    'secret': process.env.secretKey,
     'database': 'mongodb://IceCold:tcoms99@ds129936.mlab.com:29936/skischool'
 }
   
