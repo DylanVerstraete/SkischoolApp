17ba74fa58b101d523673d6b86e3b3a0dc4d81f9
DylanVerstraete <dylan_verstraete1@hotmail.com>
2018-02-05 09:45:22 +0100

    refactoring turns

--- server/controllers/turns.js
+++ server/controllers/turns.js
@@ -1,14 +1,13 @@
 const Turn = require('../app/models/turn')
 
 function edit (id, minutes) {
-  Turn.findOne({
+  return Turn.findOne({
     _id: id
   }, function (err, turn) {
     if (err) return err
+    turn.minutes = (turn.minutes - minutes)
 
-    turn.minutes = minutes
-
-    if (turn.minutes === 0) {
+    if (turn.minutes === 0 || turn.minutes < 0) {
       turn.used = true
     }
 
