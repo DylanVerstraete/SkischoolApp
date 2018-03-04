3043fdaa52126e287bde095e607055e64383a8d8
DylanVerstraete <dylan_verstraete1@hotmail.com>
2018-03-04 11:53:45 +0100

    refactor

--- server/routes/setup.js
+++ server/routes/setup.js
@@ -1,9 +1,8 @@
 const router = require('express').Router()
 const setup = require('../controllers/setup')
-const passport = require('passport')
 
 /* create sample user */
-router.get('/', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+router.get('/', function (req, res, next) {
   setup.createAdminUser()
     .then(user => {
       res.json(user)
